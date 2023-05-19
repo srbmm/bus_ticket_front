c507fd823b1689a4fe07784dbbabe321379e18a6
Mohammad Sohrabi <shrbmamad@gmail.com>
2023-05-19 18:16:06 +0330

    delete testData

--- src/pages/admin/tickets.jsx
+++ src/pages/admin/tickets.jsx
@@ -11,36 +11,6 @@ import Chart from "../../components/Chart.jsx";
 import Select from "react-select";
 import student from "../../data/student.js";
 import bus from "../../data/bus.js";
-const testData = [
-    {
-        name: 'Page A',
-        uv: 4000
-    },
-    {
-        name: 'Page B',
-        uv: 3000
-    },
-    {
-        name: 'Page C',
-        uv: 2000
-    },
-    {
-        name: 'Page D',
-        uv: 2780
-    },
-    {
-        name: 'Page E',
-        uv: 1890
-    },
-    {
-        name: 'Page F',
-        uv: 2390
-    },
-    {
-        name: 'Page G',
-        uv: 3490
-    },
-];
 
 
 const Tickets = () => {
